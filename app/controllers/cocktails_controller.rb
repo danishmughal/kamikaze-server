a87f009f4ebc7ea7c1731097d620ee13c8f9f79d
Danish Mughal <mughal.danish@gmail.com>
2013-09-05 20:45:33 -0700

    ingredients in array

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -4,30 +4,39 @@ class CocktailsController < ApplicationController
   # GET /cocktails
   # GET /cocktails.json
   def index
-    input = params[:ingredients]
+    ingredients = params[:ingredients]
+    if params[:limit]
+      searchlimit = params[:limit]
+    else
+      searchlimit = '10'
+    end
 
     require 'nokogiri'
     require 'uri-handler'
     require 'open-uri'
 
-    input = input.to_uri
-    doc = Nokogiri::HTML(open("http://www.webtender.com/cgi-bin/search?name=&ingr=#{input}&what=drink&show=10&verbose=on"))
+    ingredients = ingredients.to_uri
+    doc = Nokogiri::HTML(open("http://www.webtender.com/cgi-bin/search?name=&ingr=#{ingredients}&what=drink&show=#{searchlimit}&verbose=on"))
     index = 0
 
     drinknames = []
-    ingredients = []
+    ings = []
     cocktails = Hash.new
 
     doc.css("td[width='90%']").each do |node|
       # puts node.xpath('/a').text
       drinknames[index] =  node.css('a').text
-      ingredients[index] = node.css("small")[1].text
+      ings = node.css("small")[1].text
 
-      cocktails[drinknames[index]] = [ingredients[index]]
+      ings.gsub!('Ingredients: ', '')
+      ings = ings.split(', ')
+
+
+      cocktails[drinknames[index]] = [ings]
 
       index += 1
     end
-    
+
     respond_to do |format|
       format.json { render :json => cocktails }
     end
